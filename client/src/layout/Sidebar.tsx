41d65d17d5a0ea146e51d458bafe6590bd544d2d
Karan Chourasia <kc2532002@gmail.com>
2025-07-23 18:33:02 +0530

    changes

--- client/src/layout/Sidebar.tsx
+++ client/src/layout/Sidebar.tsx
@@ -36,7 +36,7 @@ export default function SidebarLayout() {
     <div>
       <AppSidebar />
       <SidebarInset className="hidden md:block">
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className="flex h-18 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-10" />
             <Separator
