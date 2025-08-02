714f8720c136cf5efa730b6c19bd27a50b87daaf
Karan Chourasia <kc2532002@gmail.com>
2025-08-02 11:41:33 +0530

    proxy-removed

--- client/src/store/rootReducer.ts
+++ client/src/store/rootReducer.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./slices/userSlice";
 import userAuthReducer from "./slices/authSlice";
 
 const rootReducer = combineReducers({
-    user: userReducer,
     auth: userAuthReducer, 
 });
 
