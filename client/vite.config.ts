62810d3b2031154085f3fc1baecf8e57670c5ef0
Karan Chourasia <kc2532002@gmail.com>
2025-08-07 10:32:01 +0530

    deploy-test-api-2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,13 +10,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://a.aimshala.com",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://a.aimshala.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
