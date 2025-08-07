8ac2082f91c74122817f679ef5582416166fe890
Karan Chourasia <kc2532002@gmail.com>
2025-08-07 10:26:45 +0530

    deploy-test-api

--- client/src/store/rootReducer.ts
+++ client/src/store/rootReducer.ts
@@ -1,9 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
-// import userAuthReducer from "./slices/authSlice";
+import userAuthReducer from "./slices/authSlice";
 import userAssessmentReducer from "./slices/platformDesk/assessmentSlice"
 
 const rootReducer = combineReducers({
-    // auth: userAuthReducer, 
+    auth: userAuthReducer, 
     assessment: userAssessmentReducer,
 });
 
